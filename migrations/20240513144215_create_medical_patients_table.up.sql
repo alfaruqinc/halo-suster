1e23d5ea4c6f6635b8f70ecd3dfd2364efd00b65
umar006 <umaralfaruq597@gmail.com>
2024-05-13 22:06:55 +0700

    feat: medical records table migration

---
+++ migrations/20240513144215_create_medical_patients_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS medical_patients (
+    id VARCHAR(26) PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    identity_number INT NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    birth_date TIMESTAMPTZ NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    id_card_img VARCHAR NOT NULL,
+
+    CONSTRAINT indentity_number_medical_patients_unique UNIQUE(identity_number),
+    CONSTRAINT gender_medical_patients_check CHECK(gender IN ('male', 'female'))
+);
