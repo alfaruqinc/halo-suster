108c3c20be1a87e296d74e41720d329b9022dcb4
umar006 <umaralfaruq597@gmail.com>
2024-05-16 06:51:48 +0700

    feat: get all users handler

---
+++ internal/handler/user.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"health-record/internal/domain"
+	"health-record/internal/service"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type User interface {
+	GetAllUsers() fiber.Handler
+}
+
+type user struct {
+	userService service.User
+}
+
+func NewUser(userService service.User) User {
+	return &user{
+		userService: userService,
+	}
+}
+
+func (u *user) GetAllUsers() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		users, err := u.userService.GetAllUsers(ctx.Context())
+		if err != nil {
+			return ctx.Status(err.Status()).JSON(err)
+		}
+
+		resp := domain.NewStatusOK("success retrive all users", users)
+
+		return ctx.Status(resp.Status()).JSON(resp)
+	}
+}
