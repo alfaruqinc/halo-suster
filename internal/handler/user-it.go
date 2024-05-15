e5cda1fec844f8750c908d79edc5c990d6d19726
umar006 <umaralfaruq597@gmail.com>
2024-05-15 09:31:27 +0700

    feat: login handler

--- internal/handler/user-it.go
+++ internal/handler/user-it.go
@@ -11,6 +11,7 @@ import (
 
 type UserIT interface {
 	Register() fiber.Handler
+	Login() fiber.Handler
 }
 
 type userIT struct {
@@ -38,11 +39,27 @@ func (uit *userIT) Register() fiber.Handler {
 		user := body.NewUserITFromDTO()
 		resp, err := uit.userService.Register(ctx.Context(), user)
 		if err != nil {
-			return ctx.JSON(err)
+			return ctx.Status(err.Status()).JSON(err)
 		}
 
 		userCreated := domain.NewStatusCreated("success register", resp)
 
 		return ctx.Status(userCreated.Status()).JSON(userCreated)
+	}
+}
+
+func (uit *userIT) Login() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		body := new(domain.LoginUserIT)
+		ctx.BodyParser(&body)
+
+		resp, err := uit.userService.Login(ctx.Context(), *body)
+		if err != nil {
+			return ctx.Status(err.Status()).JSON(err)
+		}
+
+		userLogin := domain.NewStatusOK("success login", resp)
+
+		return ctx.Status(userLogin.Status()).JSON(userLogin)
 	}
 }
