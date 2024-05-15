f596c5b07c0ec90899a5b0567d3753ff89853ae7
umar006 <umaralfaruq597@gmail.com>
2024-05-15 08:10:55 +0700

    feat: validation error helper

---
+++ internal/helper/error.go
@@ -0,0 +1,40 @@
+package helper
+
+import (
+	"fmt"
+	"health-record/internal/domain"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func msgForTag(fe validator.FieldError) string {
+	field := fe.Field()
+	param := fe.Param()
+
+	fmt.Println(field)
+	fmt.Println(param)
+
+	switch fe.Tag() {
+	case "required":
+		return fmt.Sprintf("%s is required", field)
+	case "min":
+		return fmt.Sprintf("minimum %s is %s", field, param)
+	case "max":
+		return fmt.Sprintf("maximum %s is %s", field, param)
+	case "url", "phonenumber", "nip":
+		return fmt.Sprintf("wrong %s format", field)
+	case "oneof":
+		return fmt.Sprintf("%s should be one of this value %s", field, param)
+	}
+
+	return "unhandled validation"
+}
+
+func ValidateRequest(err error) domain.ErrorMessage {
+	if err, ok := err.(validator.ValidationErrors); ok {
+		for _, fe := range err {
+			return domain.NewErrBadRequest(msgForTag(fe))
+		}
+	}
+	return domain.NewErrBadRequest(err.Error())
+}
