d0421a6ff4fefadd8f56093134c202ae3728735d
umar006 <umaralfaruq597@gmail.com>
2024-05-16 20:34:27 +0700

    feat: get all medical records repository

--- internal/repository/medical-record.go
+++ internal/repository/medical-record.go
@@ -9,6 +9,7 @@ import (
 
 type MedicalRecord interface {
 	Create(ctx context.Context, db *sqlx.DB, record domain.MedicalRecord) (int64, error)
+	GetAllMedicalRecords(ctx context.Context, db *sqlx.DB) ([]domain.GetMedicalRecord, error)
 }
 
 type medicalRecord struct{}
@@ -32,4 +33,35 @@ func (mr *medicalRecord) Create(ctx context.Context, db *sqlx.DB, record domain.
 	}
 
 	return affRow, nil
+}
+
+func (mr *medicalRecord) GetAllMedicalRecords(ctx context.Context, db *sqlx.DB) ([]domain.GetMedicalRecord, error) {
+	query := `SELECT mr.created_at, mr.symptoms, mr.medications,
+	mp.identity_number, mp.phone_number, mp.name, mp.birth_date, mp.gender, mp.id_card_img,
+	u.id, u.nip, u.name
+	FROM medical_records mr
+	JOIN medical_patients mp ON mp.id = mr.medical_patient_id
+	JOIN users u ON u.id = mr.created_by_id`
+	rows, err := db.QueryxContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	records := []domain.GetMedicalRecord{}
+	for rows.Next() {
+		r := domain.GetMedicalRecord{}
+		err := rows.Scan(
+			&r.CreatedAt, &r.Symptoms, &r.Medications,
+			&r.IdentityDetail.IdentityNumber, &r.IdentityDetail.PhoneNumber, &r.IdentityDetail.Name,
+			&r.IdentityDetail.BirthDate, &r.IdentityDetail.Gender, &r.IdentityDetail.IDCardImg,
+			&r.CreatedBy.ID, &r.CreatedBy.NIP, &r.CreatedBy.Name,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		records = append(records, r)
+	}
+
+	return records, nil
 }
